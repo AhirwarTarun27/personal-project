9f7f8ecedb800dfb29939569c51f687d491b2d30
tarun <ahirwar.tarun@thinksys.com>
2024-09-02 12:11:59 +0530

    edited login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,50 +1,77 @@
 import React from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    height: '90vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  loginBox: {
+    width: '320px',
+    padding: '40px 20px',
+    backgroundColor: '#fff',
+    borderRadius: '8px',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+    textAlign: 'center',
+  },
+  title: {
+    marginBottom: '10px',
+    fontWeight: 700,
+    fontSize: '24px',
+  },
+  subtitle: {
+    marginBottom: '20px',
+    color: '#757575',
+  },
+  textField: {
+    marginBottom: '20px',
+    width: '100%',
+  },
+  loginButton: {
+    width: '150px',
+    marginTop: '20px',
+  },
+});
 
 const Login: React.FC = () => {
+  const classes = useStyles();
+
+  const handleLogin = () => {
+    // Handle the login logic
+  };
+
   return (
-    <Box
-      sx={{
-        height: '90vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f5f5f5',
-      }}
-    >
-      <Box
-        sx={{
-          width: '320px',
-          padding: '40px 20px',
-          backgroundColor: '#fff',
-          borderRadius: '8px',
-          boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-          textAlign: 'center',
-        }}
-      >
+    <Box className={classes.root}>
+      <Box className={classes.loginBox}>
         <Typography
+          style={{ marginBottom: 10 }}
           variant="h4"
-          sx={{ marginBottom: '10px', fontWeight: 700, fontSize: '24px' }}
+          className={classes.title}
         >
           PicShare
         </Typography>
         <Typography
+          marginBottom={'20px'}
           variant="body1"
-          sx={{ marginBottom: '20px', color: '#757575' }}
+          className={classes.subtitle}
         >
           Login to start sharing
         </Typography>
         <TextField
           variant="outlined"
           placeholder="Username"
-          fullWidth
-          sx={{ marginBottom: '20px' }}
+          className={classes.textField}
         />
         <Button
           variant="contained"
           color="primary"
-          sx={{ width: '150px', marginTop: '20px' }}
-          onClick={() => console.log('Login Clicked')}
+          className={classes.loginButton}
+          onClick={handleLogin}
+          style={{ marginTop: 20 }}
         >
           Log In
         </Button>
